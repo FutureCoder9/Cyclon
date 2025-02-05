f73d0d6db7dfcf33e4fb1cba46510f4c886a7507
Nadiya Kost <kost.nadiya@gmail.com>
2025-02-05 09:37:04 +0200

    Update update_stock.sql

--- SQL_Procedures_Triggers/triggers/update_stock.sql
+++ SQL_Procedures_Triggers/triggers/update_stock.sql
@@ -1,1 +1,9 @@
+CREATE OR REPLACE TRIGGER UpdateStock
+AFTER INSERT ON OrderDetails
+FOR EACH ROW
+BEGIN
+    UPDATE Products
+    SET StockQuantity = StockQuantity - :NEW.Quantity
+    WHERE ProductID = :NEW.ProductID;
+END;
 
