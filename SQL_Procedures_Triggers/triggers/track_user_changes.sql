a50ca1dac86cca4515f6184aed30c54213fc3ab8
Nadiya Kost <kost.nadiya@gmail.com>
2025-02-05 09:37:23 +0200

    Update track_user_changes.sql

--- SQL_Procedures_Triggers/triggers/track_user_changes.sql
+++ SQL_Procedures_Triggers/triggers/track_user_changes.sql
@@ -1,1 +1,8 @@
+CREATE OR REPLACE TRIGGER TrackUserChanges
+AFTER UPDATE ON Users
+FOR EACH ROW
+BEGIN
+    INSERT INTO UserChangeLog (UserID, OldUsername, NewUsername, ChangeDate)
+    VALUES (:OLD.UserID, :OLD.Username, :NEW.Username, CURRENT_TIMESTAMP);
+END;
 
