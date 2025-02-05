a2991ef0ba3a1e80d269e0ff65ba666825d57841
Nadiya Kost <kost.nadiya@gmail.com>
2025-02-05 09:36:00 +0200

    Update add_product.sql

--- SQL_Procedures_Triggers/procedures/add_product.sql
+++ SQL_Procedures_Triggers/procedures/add_product.sql
@@ -1,1 +1,12 @@
+CREATE OR REPLACE PROCEDURE AddProduct (
+    p_ProductName IN VARCHAR2,
+    p_VendorID IN NUMBER,
+    p_Category IN VARCHAR2,
+    p_Price IN NUMBER,
+    p_StockQuantity IN NUMBER
+) AS
+BEGIN
+    INSERT INTO Products (ProductName, VendorID, Category, Price, StockQuantity)
+    VALUES (p_ProductName, p_VendorID, p_Category, p_Price, p_StockQuantity);
+END;
 
