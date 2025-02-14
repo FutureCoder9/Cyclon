4719269b154b347c94272af8b0176f1f00eac909
Nadiya Kost <kost.nadiya@gmail.com>
2025-02-14 14:22:43 +0200

    Delete SQL_Procedures_Triggers/triggers/update_stock.sql

--- SQL_Procedures_Triggers/triggers/update_stock.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER UpdateStock
-AFTER INSERT ON OrderDetails
-FOR EACH ROW
-BEGIN
-    UPDATE Products
-    SET StockQuantity = StockQuantity - :NEW.Quantity
-    WHERE ProductID = :NEW.ProductID;
-END;
-
